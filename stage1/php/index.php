269430af501d735b77a6b0631cefb0e8140e3a1e
lekan koku <kokulekan23@gmail.com>
2018-04-11 07:24:18 +0100

    stage2

---
+++ stage1/php/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Stage 1</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <div>
+    	<p>welcome to the hng internship</p><br>
+    	<p><?php echo date("l h:i  d F o");?></p>
+    	
+    </div>
+</body>
+</html>
